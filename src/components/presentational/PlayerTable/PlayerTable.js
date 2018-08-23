87d8692d9b5467448f012d342d117062bf32c71c
Daniel Douangkesone <karateporkchop@gmail.com>
2018-08-23 10:59:46 -0500

    forgot to add file

--- src/components/presentational/PlayerTable/PlayerTable.js
+++ src/components/presentational/PlayerTable/PlayerTable.js
@@ -1,31 +1,58 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
-import Proptypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react'
+import { Table } from 'react-bootstrap'
+import Proptypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import PlayerRow from '../PlayerRow';
+import PlayerRow from '../PlayerRow'
 
-const PlayerTable = ({ players }) => {
+const PlayerTable = ({ players, toggleSortOrder }) => {
   return (
     <Table responsive>
       <thead>
         <tr>
-          <th>Name <FontAwesomeIcon icon="sort" /></th>
+          <th>
+            Name
+            <button
+              onClick={() => {
+                toggleSortOrder('name')
+              }}
+            >
+              <FontAwesomeIcon icon="sort" />
+            </button>
+          </th>
           <th>Position</th>
           <th>Team</th>
-          <th>Height <FontAwesomeIcon icon="sort" /></th>
-          <th>Weight <FontAwesomeIcon icon="sort" /></th>
+          <th>
+            Height{' '}
+            <button
+              onClick={() => {
+                toggleSortOrder('height')
+              }}
+            >
+              <FontAwesomeIcon icon="sort" />
+            </button>
+          </th>
+          <th>
+            Weight
+            <button
+              onClick={() => {
+                toggleSortOrder('weight')
+              }}
+            >
+              <FontAwesomeIcon icon="sort" />
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>
         {players.map(player => <PlayerRow player={player} key={player.id} />)}
       </tbody>
     </Table>
-  );
+  )
 }
 
 PlayerTable.propTypes = {
   players: Proptypes.array,
 }
 
-export default PlayerTable;
+export default PlayerTable
